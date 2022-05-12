70789096939a4d21a7cd1dc7ba73423f4a666581
Trung Nguyên <1851050103nguyen@ou.edu.vn>
2022-05-13 02:47:53 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
   titleHeader: "Anh thích em rồi đó, làm người yêu anh nha!",
   descriptionHeader:
-    " Còn em thì sao cho anh câu trả lời đi nào! </br> Nếu em thoát ra và không trả lời thì có nghĩa là em đã ĐỒNG Ý rồi đấy ^^",
+    " Còn em thì sao cho anh câu trả lời đi nào! Nếu em thoát ra và không trả lời thì có nghĩa là em đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  titleModar: "Cảm ơn em vì đồng ý làm người yêu của anh",
   descriptionModar:
     "I LOVE YOU",
 };
